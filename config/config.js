9bcd62645e495819a8574034bdfba118e63f9245
Chetan Godhani <cdgodhani@gmail.com>
2024-05-13 21:35:37 +0530

    Updated env

--- config/config.js
+++ config/config.js
@@ -3,21 +3,21 @@ require('dotenv').config();
 const config = {
   development: {
     jwtSecret: process.env.JWT_SECERT,
-    corsAllowOrigins: process.env.CORS_ORIGINS,
+    corsAllowOrigins: [process.env.CORS_ORIGINS],
     port: process.env.PORT || 8080,
     dbUrl: process.env.DBURL,
     cookieSessionSecret: process.env.COOKER_SECRET,
   },
   local: {
     jwtSecret: process.env.JWT_SECERT,
-    corsAllowOrigins: process.env.CORS_ORIGINS,
+    corsAllowOrigins: [process.env.CORS_ORIGINS],
     port: process.env.PORT || 8080,
     dbUrl: process.env.DBURL,
     cookieSessionSecret: process.env.COOKER_SECRET,
   },
   production: {
     jwtSecret: process.env.JWT_SECERT,
-    corsAllowOrigins: process.env.CORS_ORIGINS,
+    corsAllowOrigins: [process.env.CORS_ORIGINS],
     port: process.env.PORT || 8080,
     dbUrl: process.env.DBURL,
     cookieSessionSecret: process.env.COOKER_SECRET,
